d210190dd15b11200ec2f2a2d9abb0d9a5465984
Hamed AMZ <amzhamed@gmail.com>
2021-08-28 15:24:02 +0430

    remove selection sort and add merge sort

--- src/Sorter.java
+++ src/Sorter.java
@@ -13,17 +13,49 @@ public class Sorter {
     }
 
     public void sortGivenArray(){
-        for (int i = 0; i < inputArray.size() - 1; i++)
-        {
-            int index = i;
-            for (int j = i + 1; j < inputArray.size(); j++){
-                if (inputArray.get(j) < inputArray.get(index)){
-                    index = j;
-                }
-            }
-            int smallerNumber = inputArray.get(index);
-            inputArray.set(index, inputArray.get(i));
-            inputArray.set(i, smallerNumber);
-        }
+       divide(0, this.inputArray.size()-1);
     }
+
+   public void divide(int startIndex,int endIndex){
+       if(startIndex<endIndex && (endIndex-startIndex)>=1){
+           int mid = (endIndex + startIndex)/2;
+           divide(startIndex, mid);
+           divide(mid+1, endIndex);
+           merger(startIndex,mid,endIndex);
+       }
+   }
+
+   public void merger(int startIndex,int midIndex,int endIndex){
+       ArrayList<Integer> mergedSortedArray = new ArrayList<>();
+
+       int leftIndex = startIndex;
+       int rightIndex = midIndex+1;
+
+       while(leftIndex<=midIndex && rightIndex<=endIndex){
+           if(inputArray.get(leftIndex)<=inputArray.get(rightIndex)){
+               mergedSortedArray.add(inputArray.get(leftIndex));
+               leftIndex++;
+           }else{
+               mergedSortedArray.add(inputArray.get(rightIndex));
+               rightIndex++;
+           }
+       }
+
+       while(leftIndex<=midIndex){
+           mergedSortedArray.add(inputArray.get(leftIndex));
+           leftIndex++;
+       }
+
+       while(rightIndex<=endIndex){
+           mergedSortedArray.add(inputArray.get(rightIndex));
+           rightIndex++;
+       }
+
+       int i = 0;
+       int j = startIndex;
+       while(i<mergedSortedArray.size()){
+           inputArray.set(j, mergedSortedArray.get(i++));
+           j++;
+       }
+   }
 }
